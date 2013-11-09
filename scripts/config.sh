d8f110dda9e3fd8445e586232d04bc19a2b6d974
Paul Pruitt <paul@paulpruitt.org>
2013-11-09 15:17:26 -0500

    Added script to configure openvpn

--- scripts/config.sh
+++ scripts/config.sh
@@ -6,3 +6,11 @@ SSH_PORT=22760
 # Where should openvpn listen
 OPENVPN_PORT=22761
 
+# OpenVPN config directory
+OPENVPN_DEST_DIR=/etc/openvpn
+
+# Location of our easy-rsa installation
+EASY_RSA_DIR="${OPENVPN_DEST_DIR}/easy-rsa"
+
+# Source location of easy-rsa (docs recommend making our own copy)
+EASY_RSA_SRC_DIR="/usr/share/doc/openvpn/examples/easy-rsa/2.0"
---
+++ scripts/configure_openvpn.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+###############################################################################
+# Configures OpenVPN
+###############################################################################
+. util.sh
+
+checkroot
+
+SRC_CONF=../conf/etc/openvpn/server.conf
+TA="${OPENVPN_DEST_DIR}/ta.key"
+DH="${OPENVPN_DEST_DIR}/dh2048.pem"
+VARS_SRC=../conf/etc/openvpn/easy-rsa/vars
+
+log "Installing openvpn configuration in ${OPENVPN_DEST_DIR}"
+install -v --backup -g root -o root -m 600 -p -t ${OPENVPN_DEST_DIR} ${SRC_CONF} ||
+{
+    log "Failed to install openvpn configuration."
+    exit 2
+}
+
+# Generate DH key if needed
+if [ ! -f ${DH} ] ; then 
+    log "Creating ${DH}"
+    openssl dhparam -out ${DH} 2048 ||
+    {
+        log "Failed to create ${DH}"
+        exit 3
+    }
+else
+    log "${DH} exists. Not generating."
+fi
+
+# Generate HMAC key if needed
+if [ ! -f ${TA} ] ; then 
+    log "Creating ${TA}"
+    openvpn --genkey --secret ${TA} ||
+    {
+        log "Failed to create ${TA}"
+        exit 4
+    }
+else
+    log "${TA} exists. Not generating."
+fi
+
+if [ ! -d ${EASY_RSA_DIR} ] ; then
+   log "Installing easy-rsa from ${EASY_RSA_SRC_DIR}"
+
+   [ -d ${EASY_RSA_SRC_DIR} ] || 
+   { 
+       log "Can't find easy-rsa." 
+       exit 5 
+   }
+
+   # Install easy-rsa
+   cp -dpr ${EASY_RSA_SRC_DIR} ${EASY_RSA_DIR} ||
+   { 
+       log "Failed to install easy-rsa."
+       exit 6
+   }   
+
+   install -v --backup -g root -o root -m 0600 -p -t ${EASY_RSA_DIR} ${VARS_SRC} ||
+   {
+       log "Failed to install vars file."
+       exit 7
+   }
+else
+   log "easy-rsa exists. Not installing."  
+fi
+
+if [ ! -f ${OPENVPN_DEST_DIR}/server.key ] ; then
+    log "server key does not exist. Creating CA."
+    pushd ${EASY_RSA_DIR}
+.   ./vars
+    ./clean-all
+    ./build-ca
+    ./build-key-server server
+    popd
+
+    cp ${EASY_RSA_DIR}/keys/server.crt \
+       ${EASY_RSA_DIR}/keys/server.key \
+       ${EASY_RSA_DIR}/keys/ca.crt \
+       ${OPENVPN_DEST_DIR} || 
+    {
+        log "Failed to install openvpn keys."
+        exit 8
+    }
+else
+    log "Server key exists. Not going to mess with them."
+fi   
+
+
