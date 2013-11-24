4622edb56f93699653ad507875046d932174413b
Paul Pruitt <paul@paulpruitt.org>
2013-11-23 19:57:24 -0500

    Added script to initialize django project

---
+++ scripts/init_django_project.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Creates fresh django project
+
+usage() 
+{
+    echo "${0} project_name dest_dir"
+}
+
+projname="${1}"
+destdir="${2}"
+
+[ -z "${projname}" ] && { echo "No project name given"; usage; exit 2; }
+
+[ -z "${VIRTUAL_ENV}" ] && { echo "You're not in a virtual env." ; exit 1; }
+echo "Using virtualenv ${VIRTUAL_ENV}"
+
+django-admin.py startproject ${projname} ${destdir} || 
+{
+    echo "Failed to create projet ${projname}"
+    exit 3
+}
+
