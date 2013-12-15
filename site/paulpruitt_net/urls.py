198c747921f513effb6dad319f64ff1edd40e588
Paul Pruitt <paul@paulpruitt.org>
2013-12-15 15:25:16 -0500

    Rollback to basic bare django project. Added pblog app skeleton.

--- site/paulpruitt_net/urls.py
+++ site/paulpruitt_net/urls.py
@@ -1,19 +1,12 @@
 from django.conf.urls import patterns, include, url
-from django.conf.urls.defaults import *
-from django.conf.urls.i18n import i18n_patterns
-from django.contrib import admin
-from django.conf import settings
 
+from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = i18n_patterns('',
-    url(r'^adm/',    include(admin.site.urls)),
-    url(r'^',        include('cms.urls')),
-)
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'paulpruitt_net.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
 
-if settings.DEBUG:
-    urlpatterns = patterns('',
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-            url(r'', include('django.contrib.staticfiles.urls')),
-    ) + urlpatterns
+    url(r'^admin/', include(admin.site.urls)),
+)
